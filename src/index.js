5a48d629be6bfe1c84385c133257c6f197e57d7a
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-08-12 14:00:17 -0700

    JSX

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
-//-----------------------------------------------------------------------------Sin React
-
-// const element = document.createElement('h1') //Crear un Tipo de elemento
-// element.innerText = 'Hello, Platzi Badges' //Texto del elemento
-// const container = document.getElementById('app') //Donde poner el elemento
-
-// container.appendChild(element) //Agregar el elemento al contenedor
-
-//----------------------------------------------------------------------------- React
-
 import React from "react"; //Importar React = Create Element
 import ReactDOM from "react-dom"; //Importar ReactDOM = appendChild
 
-//Para poder poner codigo JSX es necesario importar React
+const name = "Lenika";
+const sum = () => 3 + 3;
 
-const element = <h1>Hello, Platzi Badges from React</h1>; // Creacion el elemento
+//const jsx = <h1>Hello, Platzi Badges from React</h1>; //JSX
+
+//---------------------------------------Alternativa a jsx - React.CreateElement
+
+//const element = React.createElement('_Tipo de Elemento_', {_Argumentos(Propiedades)_}, '_Children(Contenido)_')
+
+//-------------------------------------------------------------------- REACT.CREATEELEMENT
+
+// const element = React.createElement(
+//   "a",
+//   { href: "https://platzi.com" },
+//   "Ir a Platzi"
+// );
+
+//const element = React.createElement("h1", {}, `Hola, soy ${name}`);
+
+//------------------------------------------------------------------- JSX
+//const jsx = <h1>Hola soy, {_expresiones_}</h1>; //JSX
+//const jsx = <h1>Hola soy, {name}</h1>;
+//const jsx = <h1>Hola soy, {sum()}</h1>;
+
+//------------------------------------------------------------------- JSX
+
+const jsx = (
+  <div>
+    <h1>Hola, soy Lenika</h1>
+    Soy Ing. Sistemas Computacionales
+  </div>
+);
+
+//------------------------------------------------------------------- REACT.CREATEELEMENT
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola, soy Lenika"),
+  React.createElement("p", {}, "Soy Ing. Sistemas Computacionales")
+);
+
 const container = document.getElementById("app"); //Donde poner el elemento
 
 //ReactDOM.render(__que renderizar__,__Donde renderizar__)
