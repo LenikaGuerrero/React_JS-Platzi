0c6536ac0d3d41acddca8a88e3db89f4ee484777
Lenika Guerrero <guerrerov.lenika@sonora.edu.mx>
2020-09-08 12:55:44 -0700

    Ciclo de Vida de un Componente

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,41 +5,76 @@ import BadgesList from "../components/BadgesList";
 import { Link } from "react-router-dom";
 
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-        firstName: "Freda",
-        lastName: "Grady",
-        email: "Leann_Berge@gmail.com",
-        jobTitle: "Legacy Brand Director",
-        twitter: "FredaGrady22221-7573",
-        avatarUrl:
-          "https://image.freepik.com/vector-gratis/cute-golden-retriever-cachorro-patas-sobre-pared_42750-615.jpg",
-      },
-      {
-        id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-        firstName: "Major",
-        lastName: "Rodriguez",
-        email: "Ilene66@hotmail.com",
-        jobTitle: "Human Research Architect",
-        twitter: "ajorRodriguez61545",
-        avatarUrl:
-          "https://image.freepik.com/vector-gratis/lindo-perro-husky-siberiano-patas-sobre-dibujos-animados-pared_42750-520.jpg",
-      },
-      {
-        id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-        firstName: "Daphney",
-        lastName: "Torphy",
-        email: "Ron61@hotmail.com",
-        jobTitle: "National Markets Officer",
-        twitter: "DaphneyTorphy96105",
-        avatarUrl:
-          "https://image.freepik.com/vector-gratis/gracioso-perro-jack-russell-terrier-sonriendo-sobre-dibujos-animados-pared_42750-681.jpg",
-      },
-    ],
-  };
+  constructor(props) {
+    //recibe props
+    super(props); //inicializar la super clase
+    console.log("1. constructor()");
+    this.state = {
+      data: [],
+    };
+  }
+  //Montaje
+  componentDidMount() {
+    console.log("3. componentDidMount()");
+    //simulacion a api
+    this.timeoutId = setTimeout(() => {
+      this.setState({
+        data: [
+          {
+            id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
+            firstName: "Freda",
+            lastName: "Grady",
+            email: "Leann_Berge@gmail.com",
+            jobTitle: "Legacy Brand Director",
+            twitter: "FredaGrady22221-7573",
+            avatarUrl:
+              "https://image.freepik.com/vector-gratis/cute-golden-retriever-cachorro-patas-sobre-pared_42750-615.jpg",
+          },
+          {
+            id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
+            firstName: "Major",
+            lastName: "Rodriguez",
+            email: "Ilene66@hotmail.com",
+            jobTitle: "Human Research Architect",
+            twitter: "ajorRodriguez61545",
+            avatarUrl:
+              "https://image.freepik.com/vector-gratis/lindo-perro-husky-siberiano-patas-sobre-dibujos-animados-pared_42750-520.jpg",
+          },
+          {
+            id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
+            firstName: "Daphney",
+            lastName: "Torphy",
+            email: "Ron61@hotmail.com",
+            jobTitle: "National Markets Officer",
+            twitter: "DaphneyTorphy96105",
+            avatarUrl:
+              "https://image.freepik.com/vector-gratis/gracioso-perro-jack-russell-terrier-sonriendo-sobre-dibujos-animados-pared_42750-681.jpg",
+          },
+        ],
+      });
+    }, 3000);
+  }
+  //Actualizacion
+  componentDidUpdate(prevProps, prevState) {
+    console.log("5. componentDidUpdate()");
+    console.log({
+      prevProps: prevProps,
+      prevState: prevState,
+    });
+
+    console.log({
+      props: this.props,
+      state: this.state,
+    });
+  }
+  //Desmontaje
+  componentWillUnmount() {
+    console.log("6. componentWillUnmount()");
+    clearTimeout(this.timeoutId); //cancela el trabajo que tiene asignado el id
+  }
+
   render() {
+    console.log("2/4. render()");
     return (
       <React.Fragment>
         <div className="Badges">
